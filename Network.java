504f058a1905da40291f3bcb120bb3f9fe944d51
Liron Moraz <liron.moraz@post.runi.ac.il>
2026-01-09 15:36:59 +0200

    Network3

--- Network.java
+++ Network.java
@@ -55,12 +55,20 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        if (name1.equals(name2)) {
+        if (name1 == null || name2 == null || name1.equals(name2)) {
             return false;
         }
         User user1 = getUser(name1);
         User user2 = getUser(name2);
-        if (user1 == null || user2 == null || user1.follows(name2)) {
+
+        if (user1 == null || user2 == null) {
+            return false;
+        }
+
+        if (user1.follows(name2)) {
+            return false;
+        }
+        if (name1.equals(name2)) {
             return false;
         }
         return user1.addFollowee(name2);
